aeefa40c4aadeb7716461cc6e2d78ea6a7c6df82
Jamoliddin <jamoliddin0968@gmail.com>
2025-10-10 20:30:04 +0500

    update code

--- config/urls.py
+++ config/urls.py
@@ -2,9 +2,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 from documents.views import upload_docx
-
+from django.contrib import admin
+# from django.conf
 urlpatterns = [
-    path('', upload_docx, name='upload_docx'),
+    path('document', upload_docx, name='upload_docx'),
+   
+path('admin/', admin.site.urls),
 ]
 
 if settings.DEBUG:
