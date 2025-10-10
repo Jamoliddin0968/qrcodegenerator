2943674c8126a2ff717816a06a2ea3244c5f99c1
Jamoliddin <jamoliddin0968@gmail.com>
2025-10-10 20:44:27 +0500

    update admin panel

--- documents/admin.py
+++ documents/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import UploadedFile
+
+@admin.register(UploadedFile)
+class myadmin(admin.ModelAdmin):
+    list_display = ['id','uuid_name','created_ats']
