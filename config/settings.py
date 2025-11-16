88c4f95ddbbc6ebdab92a2136052677e8aa2afbc
Jamoliddin <jamoliddin0968@gmail.com>
2025-11-16 14:33:05 +0500

    bug fix

--- config/settings.py
+++ config/settings.py
@@ -126,7 +126,8 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
+
 MEDIA_URL = 'media/'
 STATIC_URL = 'static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
